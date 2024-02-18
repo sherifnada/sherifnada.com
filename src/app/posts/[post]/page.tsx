a61c22d257f86adfd874ab483d5c289cc22c9a1d
Sherif Nada <snadalive@gmail.com>
2024-02-17 16:33:28 -0800

    replace react-markdown with next-mdx-remote bc it's simpler

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -1,42 +1,14 @@
 import { POSTS } from "@/utils/contentService";
-import Markdown from "react-markdown";
-import {Prism as SyntaxHighlighter} from "react-syntax-highlighter";
-import {vs} from "react-syntax-highlighter/dist/esm/styles/hljs";
-import remarkGfm from "remark-gfm";
-
-const CodeBlock = (props: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>) => {
-    const {className, children, ref, ...rest} = props;
-    const match = /language-(\w+)/.exec(className || '')
-    if (match){
-        return (<div>
-            <SyntaxHighlighter
-                {...rest}
-                children={String(children).replace(/\n$/, '')}
-                language={match[1]}
-                style={vs}
-            />
-        </div>)
-    } else  {
-        return <code {...rest}>
-            {children}
-        </code>
-    }
-};
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
 export default function Page({params} : {params: {post: string}}) {
     const post = POSTS[params.post];
 
     return (
-        <Markdown
-            className="markdown prose"
-            children={post.content}
-            remarkPlugins={[remarkGfm]}
-            components={{
-                code(props){
-                    return CodeBlock(props);
-                }
-            }}
-        >
-        </Markdown>
-    )
+        <div className="">
+            <div className="pb-20 prose">
+                <MDXRemote source={post.content} />
+            </div>
+        </div>
+    );   
 }
