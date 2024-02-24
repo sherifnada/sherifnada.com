37c109679b84263cdaa24a2bcda31622c8da03d8
Sherif Nada <snadalive@gmail.com>
2024-02-24 04:17:07 -0600

    make email form a lot simpler (and dumber)

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -1,44 +1,36 @@
-import {crimsonText} from '@/utils/fonts';
-import Script from 'next/script';
+"use client";
+
+function isValidEmail(email: string) {
+    // Regular expression for basic email validation
+    const regex = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return regex.test(email);
+}
+
+
 
 export default function Page(){
     return (
         <>
-            
-            <div id="mc_embed_shell">
-                <link href="https://cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css" />
-                <style type="text/css">
-                        {`#mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
-                        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-                        We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */`}
-                </style>
-                <div id="mc_embed_signup">
-                    <form action="https://gmail.us21.list-manage.com/subscribe/post?u=81970368da0e2130fb003359e&amp;id=a9cfdaf9f5&amp;f_id=0021e8e6f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
-                        <div id="mc_embed_signup_scroll">
-                            <h2 className={`${crimsonText.className}`}>Subscribe to my newsletter</h2>
-                            <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
-                            <div className="mc-field-group">
-                                <label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span></label>
-                                <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required={true} />
-                            </div>
-                        </div>    
-                        <div id="mce-responses" className="clear foot">
-                            <div className="response" id="mce-error-response" style={{display: 'none'}}></div>
-                            <div className="response" id="mce-success-response" style={{display: 'none'}}></div>
-                        </div>
-                    <div className='hidden' style={{position: 'absolute', left: '-5000px'}}>
-                        {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
-                        <input type="text" name="b_81970368da0e2130fb003359e_a9cfdaf9f5" tabIndex={-1}/>
+
+            <div className='w-[300px]'>
+                <form onSubmit={(e) => {
+                    const formData = new FormData(e.target as HTMLFormElement);
+                    const emailValue = formData.get('EMAIL') as string;
+                    
+                    if(!emailValue || !isValidEmail(emailValue)) {
+                        e.preventDefault();
+                        alert('Please enter a valid email address.');
+                    }
+                }} action="https://gmail.us21.list-manage.com/subscribe/post?u=81970368da0e2130fb003359e&amp;id=a9cfdaf9f5&amp;f_id=0021e8e6f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank">
+                    <div className='flex flex-col'>
+                        <label className='text-xl' htmlFor='mce-EMAIL'>Email Address*</label>
+                        <input type="email" name="EMAIL" className="pl-1 border-solid border rounded border-black" id="mce-EMAIL" required={true} />
+                        <input className="hidden" type="text" name="b_81970368da0e2130fb003359e_a9cfdaf9f5" tabIndex={-1}/>
                     </div>
-                        <div className="optionalParent">
-                            <div className="clear foot">
-                                <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" readOnly /> 
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <Script type="text/javascript" src="https://s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></Script>
-                <Script type="text/javascript" src="/mailchimp-ftypes.js"></Script>
+                    <div className="mt-5 w-32 cursor-pointer text-center bg-transparent hover:bg-text-primary text-text-primary font-semibold hover:text-white py-2 px-4 border border-text-primary hover:border-transparent rounded">
+                        <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="" value="Subscribe" readOnly /> 
+                    </div>
+                </form>
             </div>
         </>
     )    
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "text-primary": "var(--theme-text-color)",
+        "bg-primary": "var(--theme-background)"
+      }
     },
   },
   plugins: [
